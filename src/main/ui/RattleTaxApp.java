5c8a9519f326650f3fea0af79ff6dfe35b1ed5a8
jamesw679 <jameswu0414@gmail.com>
2023-04-01 14:09:09 -0700

    Added UI

---
+++ src/main/ui/RattleTaxApp.java
@@ -0,0 +1,30 @@
+package ui;
+
+import javax.swing.*;
+
+public class RattleTaxApp {
+
+
+    private JPanel mainPanel;
+    private JFrame mainFrame;
+    private JTextField mainInput;
+
+
+    public RattleTaxApp() {
+
+        mainPanel = new JPanel();
+        mainFrame = new JFrame("RattleTax");
+        mainFrame.setSize(500,250);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.add(mainPanel);
+
+        mainPanel.setLayout(null);
+        mainInput = new JTextField();
+        mainInput.setBounds(150,150,200,25);
+        mainPanel.add(mainInput);
+
+        mainFrame.setVisible(true);
+
+    }
+
+}
