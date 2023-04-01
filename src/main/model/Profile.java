8a94a7ce95875ae8fd73c650bc479dea678a89f2
racheljune2004@gmail.com <racheljune2004@gmail.com>
2023-04-01 14:34:11 -0700

    set up profile and T4

--- src/main/model/Profile.java
+++ src/main/model/Profile.java
@@ -1,4 +1,46 @@
 package model;
 
+// Represents profile of user who is filing T4
 public class Profile {
+    private String firstName;
+    private String lastName;
+    private int sin;
+    private String dob;
+    private String address;
+    private Boolean marital;
+
+    public Profile(String firstName, String lastName, int sin, String dob, String address, Boolean marital) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.sin = sin;
+        this.dob = dob;
+        this.address = address;
+        this.marital = marital;
+    }
+
+    // getters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getSin() {
+        return sin;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Boolean getMarital() {
+        return marital;
+    }
+
 }
